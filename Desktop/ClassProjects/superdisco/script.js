fec662db9b681f0aaee926412b50665398e86252
Ivy Stephenson <istephensondev@gmail.com>
2021-02-12 15:05:08 -0600

    first commit

--- Desktop/ClassProjects/superdisco/script.js
+++ Desktop/ClassProjects/superdisco/script.js
@@ -1,37 +1,49 @@
-var currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+var emailInput = document.querySelector('#email');
+var passwordInput = document.querySelector('#password');
+var signUpButton = document.querySelector('#sign-up');
+var msgDiv = document.querySelector('#msg');
+var userEmailSpan = document.querySelector('#user-email');
+var userPasswordSpan = document.querySelector('#user-password');
 
+function renderLastRegistered() {
+  // Retrieve the last email and password from localStorage using `getItem()`
+  var email = localStorage.getItem('email');
+  var password = localStorage.getItem('password');
 
-document.getElementById("currentDay").innerHTML = currentDay;
+  // If they are null, return early from this function
+  if (email === null || password === null) {
+    return;
+  }
 
-
-var nine = moment().hour(9)
-
-
-
-
-
-function colorUpdater() {
-    var currentTime = moment().hours()
-    $('.time-block').each(function () {
-        var tabTime = parseInt($(this).attr("hour"))
-        if (tabTime < currentTime) {
-            $(this).addClass("past")
-        }
-        else if (tabTime === currentTime) {
-            $(this).removeClass("past")
-            $(this).addClass("present")
-        }
-        else {
-            $(this).removeClass("past")
-            $(this).removeClass("present")
-            $(this).addClass("future")
-        }
-    })
+  // Set the text of the 'userEmailSpan' and 'userPasswordSpan' to the corresponding values from localStorage
+  userEmailSpan.textContent = email;
+  userPasswordSpan.textContent = password;
 }
-colorUpdater()
 
-var description = $(this).siblings(".description").val()
-var time = $
-console.log(description)
+renderLastRegistered();
 
-localStorage.setItem(time, text);
+function displayMessage(type, message) {
+  msgDiv.textContent = message;
+  msgDiv.setAttribute('class', type);
+}
+
+signUpButton.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  var email = document.querySelector('#email').value;
+  var password = document.querySelector('#password').value;
+
+  if (email === '') {
+    displayMessage('error', 'Email cannot be blank');
+  } else if (password === '') {
+    displayMessage('error', 'Password cannot be blank');
+  } else {
+    displayMessage('success', 'Registered successfully');
+
+    // Save email and password to localStorage using `setItem()`
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+    // Render the last registered email and password
+    renderLastRegistered();
+  }
+});
